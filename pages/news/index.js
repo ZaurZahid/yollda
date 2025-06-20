0eb2962372f021802ed3047d87d95ee6d573b27b
Zaur Aliyev <zauraliyevturnup@gmail.com>
2025-06-20 17:43:27 +0400

    to ltr

---
+++ pages/news/index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import BlogsPage from "../../src/components/blogs/BlogsPage";
+import Layout from "../../src/components/layout/Layout";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+import { fetchFromAPI } from "../../src/hooks/apiFetcher";
+
+export default function News({ /* siteData, blogsData, */ error }) {
+    const { t } = useTranslation('common')
+
+    if (error) {
+        return <h1>{error}</h1>;
+    }
+
+    return (
+        <Layout /* siteData={siteData} */ theme={'transparent'}>
+            <Head>
+                <html dir={'ar' === 'az' ? 'rtl' : 'ltr'} />
+                <title>Yollda | {t('navigation.blog')}</title>
+                <meta name="description" content="This is a description of blog page." />
+            </Head>
+
+            <img
+                src="/frame.png"
+                alt="Beautiful image"
+                class="h-[530px] lg:h-[590px] w-full object-cover -mt-20"
+            />
+            <BlogsPage blogsData={[]/* blogsData */} />
+        </Layout>
+    );
+}
+
+export async function getServerSideProps({ locale }) {
+    try {
+        // const [siteData, blogsData] = await Promise.all([
+        //     fetchFromAPI('/api/v1/support/site/', locale),
+        //     fetchFromAPI('/api/v1/support/blog/?page=1&per_page=10', locale),
+        // ]);
+        return {
+            props: {
+                ...(await serverSideTranslations(locale, ['common'])),
+                // siteData,
+                // blogsData,
+            },
+        };
+    } catch (error) {
+        console.error('Error in getServerSideProps:', error);
+
+        return {
+            props: {
+                ...(await serverSideTranslations(locale, ['common'])),
+                siteData: null,
+                blogsData: null,
+                error: 'Failed to load data.',
+            },
+        };
+    }
+}
